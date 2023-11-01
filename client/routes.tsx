edbfd35114874283e0a63a34735a4e4b7e2ca471
Suwon Jeong <integer.won@gmail.com>
2023-11-02 00:49:22 +1300

    Add review component

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,10 +2,12 @@ import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import App from './components/App.tsx'
 import ReviewList from './components/ReviewList.tsx'
+import Review from './components/Review.tsx'
 
 export const routes = createRoutesFromElements(
   <>
     <Route path="/" element={<App />} />
     <Route path="/reviews" element={<ReviewList />} />
+    <Route path="/reviews/:id" element={<Review/>} />
   </>
 )
