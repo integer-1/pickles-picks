1af8f69e73e62b795db8ddaa03eb4f0b24e33cf7
Benedict Velasco <benedict.velasco@outlook.com>
2023-11-01 16:16:49 +1300

    fixed routing issue

--- client/routes.tsx
+++ client/routes.tsx
@@ -4,7 +4,8 @@ import App from './components/App.tsx'
 import ReviewList from './components/ReviewList.tsx'
 
 export const routes = createRoutesFromElements(
-  <Route path="/" element={<App />}>
+  <>
+    <Route path="/" element={<App />} />
     <Route path="/reviews" element={<ReviewList />} />
-  </Route>
+  </>
 )
