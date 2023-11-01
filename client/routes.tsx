1639354d023ac6513ee002f5b6578b91554f1a81
Jayde Medder <jayde.medder@hotmail.com>
2023-11-01 14:28:34 +1300

    trying to render reviewlist

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,7 +1,6 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import App from './components/App.tsx'
-import Home from './components/Home.tsx'
 import ReviewList from './components/ReviewList.tsx'
 
 export const routes = createRoutesFromElements(
