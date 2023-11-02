11a607200b6deba60a9b83f727a6d30858c309ea
Shrena Patel <shrena.p@hotmail.co.uk>
2023-11-03 11:53:31 +1300

    fix redirect after login

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -2,19 +2,15 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { useState } from 'react'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 import ReviewList from './ReviewList'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Login() {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
 
-  const navigate = useNavigate()
-  // const history = useHistory()
-
-  const handleLogin = () => {
-    loginWithRedirect()
-    navigate('/reviews')
-    // history.push('/reviews')
+  const handleLogin = async () => {
+    await loginWithRedirect()
   }
+  
   return (
     <>
       <IfAuthenticated>
