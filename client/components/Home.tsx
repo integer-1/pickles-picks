25383fd8c89bdb7276dcbb995db77e7b83af29d1
Hope Goldsack <hsgoldsack@gmail.com>
2023-11-03 10:42:05 +1300

    added pickles pic

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -8,6 +8,12 @@ function Home() {
       <div className="landing-page">
         <h2>Welcome to Pickle&apos;s Picks!</h2>
         <h3>See all my favourite places to eat!</h3>
+        <img
+          src="client/styles/MiscData/Screenshot 2023-11-03 103600.png"
+          alt="Pickles portrait"
+          width="500"
+          height="600"
+        />
         <Login />
         <Register />
         <Link to="/reviews" className="button">
